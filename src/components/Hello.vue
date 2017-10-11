6a6216f8d2b3d2f562bc2b0f57cf0b399a78e0ec
wuzhen@navinfo.com <wuzhen@navinfo.com>
2017-10-11 17:32:11 +0800

    增加文字调通效果

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <input v-model.number="number" type="number" step="20">
+    <p>{{ animatedNumber }}</p>
     <bar-poi-chart :roadData='charData.road'></bar-poi-chart>
     <bar-road-chart :roadData='charData.road'></bar-road-chart>
   </div>
@@ -10,10 +12,14 @@
 import BarRoadChart from '@/components/BarRoadChart'
 import BarPoiChart from '@/components/BarPoiChart'
 
+var TWEEN = require('@tweenjs/tween.js');
+
 export default {
   name: 'hello',
   data () {
     return {
+      number: 0,
+      animatedNumber: 0,
       msg: {},
       charData: {
         road: {
@@ -21,6 +27,25 @@ export default {
             xAxis:[]
         }
       }
+    }
+  },
+  watch: {
+    number: function(newValue, oldValue) {
+      var vm = this
+      console.info('vm', vm);
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+      new TWEEN.Tween({ tweeningNumber: oldValue })
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .to({ tweeningNumber: newValue }, 500)
+        .onUpdate(function () {
+          vm.animatedNumber = this.tweeningNumber.toFixed(0)
+        })
+        .start()
+      animate()
     }
   },
   components: {
@@ -42,7 +67,7 @@ export default {
           data.body.data.road.seriesData[4] = data.body.data.road.seriesData[4] + index;
           data.body.data.road.seriesData[9] = data.body.data.road.seriesData[9] + index;
           that.charData = data.body.data
-          that.msg = index;
+          // that.msg = index;
         })
       },2000);
     }
@@ -52,10 +77,11 @@ export default {
     let that = this;
     let index = 0;
     setInterval(function () {
-      index = index + 5;
+      index = index + 10;
       that.msg = index;
       console.info(that.msg);
-    },2000);
+      that.number = index;
+    },3000);
   }
 }
 </script>
